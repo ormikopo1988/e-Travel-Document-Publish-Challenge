bf24577e38d8de4c2d369e135d902c01a9efe1d2
Orestis Meikopoulos <diktyas1988@gmail.com>
2017-01-07 19:58:53 +0200

    Tests for services

---
+++ ETravel.DAL/IEtravelEntities.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.Entity;
+
+namespace ETravel.DAL
+{
+    public interface IEtravelEntities
+    {
+        DbSet<User> Users { get; set; }
+        DbSet<AttachmentSet> AttachmentSets { get; set; }
+        DbSet<Attachment> Attachments { get; set; }
+    }
+}
---
+++ ETravel.Server.Web.Api.Tests/Services/UserServiceTests.cs
@@ -0,0 +1,93 @@
+﻿using ETravel.BAL;
+using ETravel.BAL.Services;
+using ETravel.DAL;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Data.Entity;
+
+namespace ETravel.Server.Web.Api.Tests.Services
+{
+    [TestClass]
+    public class UserServiceTests
+    {
+        private UserService _userService;
+
+        private Mock<DbSet<User>> _mockUsers;
+        
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _mockUsers = new Mock<DbSet<User>>();
+            
+            _userService = new UserService(new UnitOfWork());
+        }
+
+        [TestMethod]
+        public void GetAllUsers_RequestForAllUsers_ShouldReturnAllRegisteredUsers()
+        {
+            //Arrange
+
+            //Act
+            var users = _userService.GetAllUsers();
+
+            users.Should().NotBeEmpty();
+        }
+
+        [TestMethod]
+        public void GetLastTenRegisteredUsers_RequestForLastTenRegisteredUsers_ShouldReturnAtMost10LastRegisteredUsers()
+        {
+            //Arrange
+
+            //Act
+            var users = _userService.GetLastTenRegisteredUsers();
+
+            users.Should().NotBeEmpty();
+            users.Count.Should().BeLessOrEqualTo(10);
+        }
+
+        [TestMethod]
+        public void GetUser_UserIdNotExists_ShouldReturnNull()
+        {
+            //Arrange
+
+            //Act
+            var user = _userService.GetUser(0);
+
+            user.Should().BeNull();
+        }
+
+        [TestMethod]
+        public void GetUser_UserExists_ShouldReturnASingleUser()
+        {
+            //Arrange
+
+            //Act
+            var user = _userService.GetUser(1);
+
+            user.Should().NotBeNull();
+        }
+
+        [TestMethod]
+        public void GetUserByUsername_UsernameNotExists_ShouldReturnNull()
+        {
+            //Arrange
+
+            //Act
+            var user = _userService.GetUserByUsername("cdsd");
+
+            user.Should().BeNull();
+        }
+
+        [TestMethod]
+        public void GetUserByUsername_UsernameExists_ShouldReturnASingleUser()
+        {
+            //Arrange
+
+            //Act
+            var user = _userService.GetUserByUsername("orme@ait.gr");
+
+            user.Should().NotBeNull();
+        }
+    }
+}
